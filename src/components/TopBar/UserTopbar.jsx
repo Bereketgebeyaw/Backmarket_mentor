0cfa88c8080a0cbc84639fde7f5b8721ff5eb8cc
Bereketgebeyaw <101499035+Bereketgebeyaw@users.noreply.github.com>
2025-01-02 11:35:45 +0300

    buyer route cahnged

--- src/components/TopBar/UserTopbar.jsx
+++ src/components/TopBar/UserTopbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+
 
 
 const UserTopbar = ({ cartCount }) => {

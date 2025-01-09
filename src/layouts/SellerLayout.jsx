bf470df2572a7dd95eace2f34473efe292f249ae
Bereketgebeyaw <101499035+Bereketgebeyaw@users.noreply.github.com>
2025-01-09 15:54:00 +0300

    seller improvment

--- src/layouts/SellerLayout.jsx
+++ src/layouts/SellerLayout.jsx
@@ -13,18 +13,21 @@ const SellerLayout = () => {
         }}
       >
         <ul>
-          <li>
-            <Link to="/seller/product-management">Product Management</Link>
-          </li>
-          <li>
-            <Link to="/seller/add-category">Add New Category</Link>
-          </li>
+        <li style={{ marginTop: '3rem', listStyleType: 'none' }}>
+              <Link to="/seller/product-management">Product Management</Link>
+            </li>
+            <li style={{ marginTop: '2rem', listStyleType: 'none' }}>
+              <Link to="/seller/add-category">Add New Category</Link>
+            </li>
+            <li style={{ marginTop: '2rem', listStyleType: 'none' }}>
+              <Link to="/seller/add-subcategory">Add New Subcategory</Link>
+            </li>
+           
 
-          {/* Add more admin routes here */}
         </ul>
       </nav>
 
-      {/* Main content area */}
+    
       <div style={{ flex: 1, padding: "20px" }}>
         <h1>Seller Dashboard</h1>
         <Outlet /> {/* Renders the specific page based on the route */}
